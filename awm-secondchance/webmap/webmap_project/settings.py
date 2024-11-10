4bed03da5c1b43e6dbf7a01e1dc38b966ddb2210
Hadassah  Joseph <105879604+HadassahJoseph@users.noreply.github.com>
2024-11-10 19:44:37 +0000

    Update settings.py

--- awm-secondchance/webmap/webmap_project/settings.py
+++ awm-secondchance/webmap/webmap_project/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+
 # Define potential paths for GDAL library
 potential_gdal_paths = [
     r"C:\Users\C21382216\AppData\Local\anaconda3\envs\awm_env\Library\bin\gdal.dll",
