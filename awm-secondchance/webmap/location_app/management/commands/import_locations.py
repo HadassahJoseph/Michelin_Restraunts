1b39f0e3bbf93dfa509634e4adacf0c072f7ca5f
Hadassah  Joseph <105879604+HadassahJoseph@users.noreply.github.com>
2024-11-10 23:03:06 +0000

    Update import_locations.py

--- awm-secondchance/webmap/location_app/management/commands/import_locations.py
+++ awm-secondchance/webmap/location_app/management/commands/import_locations.py
@@ -9,9 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         # Specify the path to your cleaned CSV file
-        with open('Michelin_Restraunts/awm-secondchance/CSV/cleaned_michelin_restaurants.csv', mode='r') as file:
-            reader = csv.DictReader(file)  # Use DictReader to handle CSV headers as field names
-
+        with open('/home/ubuntu/Michelin_Restraunts/awm-secondchance/CSV/cleaned_michelin_restaurants.csv', mode='r') as file:
             for row in reader:
                 # Extract the relevant columns from the CSV
                 name = row['name']
