0cdf8df56d3a226c9a8b403adddf98eeabd53bd4
Hadassah  Joseph <105879604+HadassahJoseph@users.noreply.github.com>
2024-11-10 19:42:21 +0000

    Update settings.py

--- awm-secondchance/webmap/webmap_project/settings.py
+++ awm-secondchance/webmap/webmap_project/settings.py
@@ -172,7 +172,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "location_app" / "static",  # This points to 'location_app/static'
 ]
 # STATIC_ROOT = BASE_DIR / 'static'  # Where collected static files will go
-STATIC_ROOT = os.path.join[ BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
